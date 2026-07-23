e5e9bed1075ae7e17d9918b5e14d49e0f6744418
jmin4078 <wpalsrl8588@gmail.com>
2026-07-23 10:34:04 +0900

    Add enrollment result view

--- src/main/java/org/example/mybatis3/controller/MainController.java
+++ src/main/java/org/example/mybatis3/controller/MainController.java
@@ -1,9 +1,13 @@
 package org.example.mybatis3.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.mybatis3.entity.Course;
+import org.example.mybatis3.entity.Student;
 import org.example.mybatis3.service.EnrollService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +21,17 @@ public class MainController {
         model.addAttribute("students", enrollService.findAllStudents());
         model.addAttribute("courses", enrollService.findAllCourses());
         return "index";
+    }
+
+    @PostMapping("/students")
+    public String addStudent(@ModelAttribute Student student) {
+        enrollService.createStudent(student);
+        return "redirect:/";
+    }
+
+    @PostMapping("/courses")
+    public String addCourse(@ModelAttribute Course course) {
+        enrollService.createCourse(course);
+        return "redirect:/";
     }
 }
